303a421a75df1bbeeac01d3131330e9bd147ed00
layonfonseca <layon.fonseca@eldorado.org.br>
2021-11-29 12:15:51 -0300

    update constants

--- app/src/main/java/com/example/firebasepushnotification/Constants.kt
+++ app/src/main/java/com/example/firebasepushnotification/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
 
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAnzjHdYo:APA91bGv3ZcWgV8WvImdS7w-1HR0Xrfy1pDJmqQSOdiktKUTemkhLwEHLrbabpBq9RFmqwLvYTaD6_MxJmkQbwT2WUG6czD1Y6rZBe8tAK8GdCnyplyHNTEIPD3c3KVxYMwRiLPtEqG8"
+        const val SERVER_KEY = "ENTER SERVER KEY HERE"
         const val CONTENT_TYPE = "application/json"
     }
 }
